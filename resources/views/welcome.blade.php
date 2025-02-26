550bf11553a5056e29c57b0206b2c196a375808a
winshuka <vinshukakannangara@gmail.com>
2025-02-26 11:41:27 +0530

    Installed tailwind css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    @vite(entrypoints: ['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <h1>Hello 3 core creators</h1>
+    <h1 class="text-3xl text-red-500 font-bold underline">Hello 3 core creators</h1>
 </body>
 </html>
