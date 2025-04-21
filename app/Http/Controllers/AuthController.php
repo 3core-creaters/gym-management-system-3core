19bf0042162dc7f1537b60e68583dbf41802b4f9
Dulanjana <dulanjananadeeshan13@gmail.com>
2025-04-21 22:05:07 +0530

    Role management and login function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
-use illuminate\Http\Request;
+use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+
 
 class AuthController extends Controller
 {
@@ -23,8 +25,8 @@ class AuthController extends Controller
             'first_name' => 'required',
             'last_name' => 'required',
             'birthday' => 'required|date',
-            'gender' => 'required',
-            'is_role' => 'required',
+            'gender' => 'required|in:0,1',
+            'is_role' => 'required|in:0,1,2',
             'email' => 'required|unique:users',
             'password' => 'required|min:6|max:20',            
         ]);
@@ -34,13 +36,46 @@ class AuthController extends Controller
         $user->last_name = trim($request->last_name);
         $user->birthday = trim($request->birthday);
         $user->email = trim($request->email);
+        $user->gender = $request->gender;
         $user->password = Hash::make($request->password);
         $user->is_role = trim($request->is_role);
         $user->remember_token = Str::random(50);
 
         $user->save();
 
-        return redirect()->route('login')->with('success', 'Registration successful! You can now log in.');
+        return response()->json(['message' => 'User registered successfully']);
+        // return redirect()->route('login')->with('success', 'Registration successful! You can now log in.');
+    }
+
+    public function userLogin(Request $request)
+    {
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password], true))
+        {
+            if(Auth::User()->is_role == 2)
+            {
+                return redirect()->intended('dashboard/adminDashboard');
+                // return response()->json(['message' => 'You are in admin dashboard']);
+            }
+            else if(Auth::User()->is_role == 1)
+            {
+                return redirect()->intended('dashboard/trainerDashboard');
+                // return response()->json(['message' => 'You are in trainer dashboard']);
+            }
+            else if(Auth::User()->is_role == 0)
+            {
+                return redirect()->intended('dashboard/userDashboard');
+                // return response()->json(['message' => 'You are in user dashboard']);
+            }
+            else
+            {
+                return redirect('login')->with('error', 'No available email.. Please check your email');
+            }
+        }
+        else
+        {
+            return redirect()->back()->with('error', 'Invalid credentials. Please try again.');
+        }
+
     }
 }
 
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,21 @@ use App\Http\Controllers\AuthController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::get('/register', [AuthController::class, 'register']);
+
+Route::post('/login', [AuthController::class, 'userLogin']);  
+Route::get('/login', [AuthController::class, 'login']);  
+
+Route::group(['middleware' => 'admin'], function(){
+
+});
+
+Route::group(['middleware' => 'trainer'], function(){
+    
+});
+
+Route::group(['middleware' => 'member'], function(){
+    
+});
